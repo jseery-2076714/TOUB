68b47548a57d77e62807f678619e477ee6f5babf
sbailey6-1877761 <81714856+sbailey6-1877761@users.noreply.github.com>
2022-05-01 15:04:20 -0700

    Window pops up

---
+++ src/mini_game.py
@@ -0,0 +1,16 @@
+#importing the important stuff
+
+import tkinter as tk
+import tkinter.font as font
+from functools import partial
+from tkinter import messagebox
+from tkinter import ttk
+
+
+#creates the window
+
+window = tk.Tk()
+window.geometry('700x800')
+window.title('Toub Game')
+window.mainloop()
+
