d64f6eaf4be4c8d71e86f6578e664fcbc8d4a22c
twoo7 <timothywoo0011@gmail.com>
2022-05-03 14:12:14 -0700

    cleaned up code and added comments

---
+++ src/convert.py
@@ -0,0 +1,7 @@
+### Coversion code will be written here
+
+def convert(value, unit1, unit2):
+    print("Converting " + str(value) + " " + str(unit1) + " to " + str(unit2))
+    ### get row associated to unit1 from Google Sheets API
+    ### convert based on retreived value
+    ### return conversion
