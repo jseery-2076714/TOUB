d12c9578b655b6a0710ef55d58249d1822047343
sbailey6-1877761 <81714856+sbailey6-1877761@users.noreply.github.com>
2022-05-05 14:05:42 -0700

    tu changed shit

--- src/mini_game.py
+++ src/mini_game.py
@@ -1,16 +1,27 @@
 #importing the important stuff
 
+
 import tkinter as tk
 import tkinter.font as font
 from functools import partial
 from tkinter import messagebox
 from tkinter import ttk
 
+def main():
+    #creates the window
+    window = tk.Tk()
+    window.geometry('700x800')
+    window.title('Toub Game')
 
-#creates the window
+    #mainloop for the window
+    window.mainloop()
 
-window = tk.Tk()
-window.geometry('700x800')
-window.title('Toub Game')
-window.mainloop()
+    #fonts 
+    font1 = font.Font('Title', font = (24, 'Arial'))
+    font2 = font.Font('Game', Font = (18, 'Arial'))
+
+if __name__ == "__main__":
+    main()
+
+
 
