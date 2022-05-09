4058f360cc6b6f73815e0b47bdf6c937520faeea
Jack Seery <jseery@uw.edu>
2022-05-09 15:24:57 -0700

    update unit tests

--- test/unit_test.py
+++ test/unit_test.py
@@ -9,16 +9,16 @@ import src
 
 
 def test_retrieve():
-    assert sheets.retrieveData('cm') == "Centimeter, 1, 1, 1"
+    assert src.sheets.retrieveData('cm') == "Centimeter, 1, 1, 1"
 
 
 def test_convert():
-    assert convert.convertUnit('1', 'inch', 'cm') == '2.54 cm'
+    assert src.convert.convertUnit('1', 'inch', 'cm') == '2.54 cm'
 
 
 def test_select():
-    assert convert.unitSelect('cm', 1) == 'Randomly selected new unit'
+    assert src.convert.unitSelect('cm', 1) == 'Randomly selected new unit'
 
 
 def test_parse():
-    assert convert.parseMessage('Message 1 cm') == 'Converted message'
+    assert src.convert.parseMessage('Message 1 cm') == 'Converted message'
