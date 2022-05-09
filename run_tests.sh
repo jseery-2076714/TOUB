7a2aa218ba537028af937169b8c2d1643a4fb82a
Jack Seery <jseery@uw.edu>
2022-05-09 14:32:20 -0700

    change token script

--- run_tests.sh
+++ run_tests.sh
@@ -5,13 +5,16 @@
 # sleep 5s
 TOKEN1='OTcwNTA5NjMzMTc5NDkyMzkz.G6sluc'
 TOKEN2='.1eJE5gh2BdyJcTHc_JHPcZe0QgEkQIepQqWNYM'
+TESTER_TOKEN1='OTcwNTI4MTAyODEzOTQ5OTky.'
+TESTER_TOKEN2='Ym9Quw.qIXzCZQpin9a-fPbfvHWwiMFwrw'
 echo "Decrypting token"
 echo 'DISCORD_TOKEN='$TOKEN1$TOKEN2 > ./src/.env
 echo 'DISCORD_SERVER=TOUB' >> ./src/.env
 echo "Running unit tests"
 pytest
 echo "Running system tests"
-python test/sys_test.py -c "$CHANNEL" -r all "$TARGET_ID" "$TESTER_TOKEN"
+python test/sys_test.py -c "$CHANNEL" -r all "$TARGET_ID" "$TESTER_TOKEN1$TESTER_TOKEN2"
+cat ./src/.env
 rm ./src/.env
 # python example_tester.py "$TARGET_ID" "$TESTER_TOKEN" -c "$CHANNEL" -r all
 
