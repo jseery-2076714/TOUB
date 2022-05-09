bbf2137bb9a69e4e599bf02f18699b969cc479ce
Jack Seery <jseery@uw.edu>
2022-05-09 15:35:33 -0700

    update unit tests

--- test/unit_test.py
+++ test/unit_test.py
@@ -1,24 +1,25 @@
 # Database/Google Sheets API/backend processing tests
 # testInsert
-import sys
-import os
-import src
+#import sys
+#import os
+from src.convert import *
+from src.sheets import *
 #sys.path.append(os.path.abspath("/home/runner/work/TOUB/TOUB/src"))
 #from convert import convertUnit, unitSelect, parseMessage
 #from sheets import retrieveData
 
 
 def test_retrieve():
-    assert src.sheets.retrieveData('cm') == "Centimeter, 1, 1, 1"
+    assert retrieveData('cm') == "Centimeter, 1, 1, 1"
 
 
 def test_convert():
-    assert src.convert.convertUnit('1', 'inch', 'cm') == '2.54 cm'
+    assert convertUnit('1', 'inch', 'cm') == '2.54 cm'
 
 
 def test_select():
-    assert src.convert.unitSelect('cm', 1) == 'Randomly selected new unit'
+    assert unitSelect('cm', 1) == 'Randomly selected new unit'
 
 
 def test_parse():
-    assert src.convert.parseMessage('Message 1 cm') == 'Converted message'
+    assert parseMessage('Message 1 cm') == 'Converted message'
