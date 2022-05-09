1826252198dc3ff517a5fb65b25e0cabcbb691fb
Jack Seery <jseery@uw.edu>
2022-05-09 15:22:38 -0700

    update unit tests

--- test/unit_test.py
+++ test/unit_test.py
@@ -2,22 +2,23 @@
 # testInsert
 import sys
 import os
-sys.path.append(os.path.abspath("/home/runner/work/TOUB/TOUB/src"))
-from convert import convertUnit, unitSelect, parseMessage
-from sheets import retrieveData
+import src
+#sys.path.append(os.path.abspath("/home/runner/work/TOUB/TOUB/src"))
+#from convert import convertUnit, unitSelect, parseMessage
+#from sheets import retrieveData
 
 
 def test_retrieve():
-    assert retrieveData('cm') == "Centimeter, 1, 1, 1"
+    assert sheets.retrieveData('cm') == "Centimeter, 1, 1, 1"
 
 
 def test_convert():
-    assert convertUnit('1', 'inch', 'cm') == '2.54 cm'
+    assert convert.convertUnit('1', 'inch', 'cm') == '2.54 cm'
 
 
 def test_select():
-    assert unitSelect('cm', 1) == 'Randomly selected new unit'
+    assert convert.unitSelect('cm', 1) == 'Randomly selected new unit'
 
 
 def test_parse():
-    assert parseMessage('Message 1 cm') == 'Converted message'
+    assert convert.parseMessage('Message 1 cm') == 'Converted message'
