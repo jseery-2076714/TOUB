1a402dd07e6d16f580b892ee4b1cd0c38bb152b7
Jack Seery <jseery@uw.edu>
2022-05-09 14:10:02 -0700

    change token script

--- run_tests.sh
+++ run_tests.sh
@@ -13,6 +13,7 @@ echo "Running unit tests"
 pytest
 echo "Running system tests"
 python test/sys_test.py -c "$CHANNEL" -r all "$TARGET_ID" "$TESTER_TOKEN"
+cat ./src/.env
 rm ./src/.env
 # python example_tester.py "$TARGET_ID" "$TESTER_TOKEN" -c "$CHANNEL" -r all
 
