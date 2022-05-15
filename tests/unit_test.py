7253e360f764e1e70f894dc7b3e393715afc0dbb
Jack Seery <jseery@uw.edu>
2022-05-15 15:30:26 -0700

    added new unit test

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -7,7 +7,7 @@ def module_directory(name_module, path):
     import_module = importlib.util.module_from_spec(P)
     P.loader.exec_module(import_module)
     return import_module
- 
+
 sheets = module_directory("sheets", "./modules/sheets.py")
 convert = module_directory("convert", "./modules/convert.py")
 sheets.set_up_api()
@@ -18,6 +18,7 @@ def test_retrieve():
 
 def test_convert():
     assert convert.convert_unit(1, 'feet', 'yard') == '0.33333333333333337 yard'
+    assert convert.convert_unit(2, 'feet', 'yard') == '0.66666666666666674 yard'
 
 
 def test_select():
