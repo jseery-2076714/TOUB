919654ae196eae2d7cfd89a2961e7f4e06e97a5b
twoo7 <62158470+twoo7@users.noreply.github.com>
2022-05-15 14:18:36 -0700

    added comments to parseMessage

--- src/convert.py
+++ src/convert.py
@@ -1,28 +1,31 @@
 # Parsing and conversion
 import random as rd
-from src import sheets
+import sheets
 
 # Given a message from Discord
 # Return converted message
 def parseMessage(message):
     # error checking
-    # ensuring message contains a value and a unit to convert
-    # check what level of chaos the bot is set to
-    # selectedNewUnit = unitSelect(parsedUnit, chaosLevel)
-    # newMessage = convert(parsedValue, parsedUnit, selectedNewUnit)
+    
+    # Split message up
     words = message.split(' ')
+    # find units of measurement in message
     for i in range(len(words)):
+        # if number found, check if unit follows after
         if(words[i].isnumeric()):
             if(i == len(words)-1):
                 break
             value = words[i]
+            # find unit
             for unit in sheets.records['Unit'].tolist():
+                # convert to different unit based on level of bot and update message
                 if(unit in words[i+1].lower()):
                     (resValue, resUnit) = convertUnit(float(value), unit, unitSelect(unit, sheets.level)).split(' ')
                     words[i] = resValue
                     words[i+1] = resUnit
                     i+=1    
                     break;      
+    # combine message back with newly converted unites
     return ' '.join(words)
 
 
@@ -38,6 +41,8 @@ def convertUnit(value, current, target):
 # Given the current unit and bot level of chaos
 # Return randomly selected unit from sheet
 def unitSelect(unit, level):
+    if(level == 1):
+        return "centimeter"
     # get possible units
     units = sheets.get_col('unit')
     # pick one
