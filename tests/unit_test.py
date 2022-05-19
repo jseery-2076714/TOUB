62cd9d77a1b76274353fe670be3cd39b1a5808c5
Jack Seery <jseery@uw.edu>
2022-05-19 13:58:32 -0700

    added another test case, changed .yml

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -14,11 +14,14 @@ convert = module_directory("convert", "./modules/convert.py")
 sheets.set_up_api()
 
 
+# Test sheets.py
 def test_retrieve():
     assert sheets.get_data('feet') == ['feet', 30.48, 929.0304, 28316.84659, 'ft']
 
 
+# Test convert.py
 def test_convert():
+    assert convert.convert_unit(0, 'feet', 'yard') == '0.0 yard'
     assert convert.convert_unit(1, 'feet', 'yard') == '0.33333333333333337 yard'
     assert convert.convert_unit(2, 'feet', 'yard') == '0.6666666666666667 yard'
 
